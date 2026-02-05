f16426f7d27f11deb3edf84c68892675952a5e8d
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2026-02-06 00:05:11 +0600

    Added `util.ParseProperties`

--- lua/includes/extensions/core/util.lua
+++ lua/includes/extensions/core/util.lua
@@ -1,7 +1,23 @@
 local Round = math.Round
 local SharedRandom = util.SharedRandom
 
- 
+local match, Explode = string.match, string.Explode
+
+
 function util.SharedRandomInt( ... )
     return Round( SharedRandom( ... ) )
+end
+
+local PATTERN_PROPERTY = "(.-)=(.-)$"
+
+function util.ParseProperties( content, fn )
+    local lines = Explode( "\n", content )
+
+    for i = 1, #lines do
+        local key, value = match( lines[i], PATTERN_PROPERTY )
+
+        if key and value then
+            fn( key, value )
+        end
+    end
 end
