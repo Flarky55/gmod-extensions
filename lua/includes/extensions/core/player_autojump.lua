29801783b4508e3e767bd54f546259d8bcd0bbf7
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2026-02-10 01:48:23 +0600

    Test if it is noticeably faster to get rid of `CMoveData:__index` calls

--- lua/includes/extensions/core/player_autojump.lua
+++ lua/includes/extensions/core/player_autojump.lua
@@ -1,9 +1,11 @@
 local PLAYER = FindMetaTable( "Player" )
 
-local META_ENTITY = FindMetaTable( "Entity" )
+local META_ENTITY       = FindMetaTable( "Entity" )
+local META_CMoveData    = FindMetaTable( "CMoveData" )
 
 local GetInfoNum, GetRunSpeed = PLAYER.GetInfoNum, PLAYER.GetRunSpeed
 local Alive, GetMoveType, WaterLevel, IsOnGround = META_ENTITY.Alive, META_ENTITY.GetMoveType, META_ENTITY.WaterLevel, META_ENTITY.IsOnGround
+local KeyDown, KeyWasDown, RemoveKey, SetMaxClientSpeed, SetMaxSpeed = META_CMoveData.KeyDown, META_CMoveData.KeyWasDown, META_CMoveData.RemoveKey, META_CMoveData.SetMaxClientSpeed, META_CMoveData.SetMaxSpeed
 local hook_Run = hook.Run
 
 local NAME_CVAR = "cl_autojump" 
@@ -26,17 +28,17 @@ end
 -- It is necessary to use exactly "Move" hook as here we can adjust player's max speed
 --  When player starts crouching, it loses its speed somewhere in the Source Engine, but here we overrides it
 hook.Add( "Move", "AutoJump", function( ply, mv )
-    if mv:KeyDown( IN_JUMP ) then
+    if KeyDown( mv, IN_JUMP ) then
         if not CanAutoJump( ply ) then return end
 
         -- `SetMaxSpeed` must be set manually in "Move" hook
         --  https://wiki.facepunch.com/gmod/CMoveData:SetMaxClientSpeed#description
         local speed = GetRunSpeed( ply )
-        mv:SetMaxClientSpeed( speed )
-        mv:SetMaxSpeed( speed )
+        SetMaxClientSpeed( mv, speed )
+        SetMaxSpeed( mv, speed )
 
-        if mv:KeyWasDown( IN_JUMP ) and IsOnGround( ply ) then
-            mv:RemoveKey( IN_JUMP )
+        if KeyWasDown( mv, IN_JUMP ) and IsOnGround( ply ) then
+            RemoveKey( mv, IN_JUMP )
         end
     end
 end )
