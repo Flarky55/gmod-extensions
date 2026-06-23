d18674e77f68ccc3b0495fec2335350840598ce9
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2026-06-23 21:37:43 +0600

    Implemented fetching player's steam profile summaries and creating SteamUser object on player

--- lua/includes/extensions/core/server/player_steamuser.lua
+++ lua/includes/extensions/core/server/player_steamuser.lua
@@ -4,5 +4,14 @@ local PLAYER = FindMetaTable( "Player" )
 
 
 hook.Add( "PlayerInitialSpawn", "SteamUser", function( ply )
-    
+    local SteamUser = { Summary = {} }
+    ply.SteamUser = SteamUser
+
+    webapi.steam.ISteamUser.GetPlayerSummaries( ply:SteamID64(), function( result )
+        if not IsValid( ply ) then return end
+
+        SteamUser.Summary = result
+
+        hook.Run( "PlayerSteamUserFetched", ply, SteamUser )
+    end )
 end )
