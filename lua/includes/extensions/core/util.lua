1578725e156796a62deca9eb173e46fa5ee0ba3f
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2026-06-23 21:36:18 +0600

    `util.ParseProperties` now trim spaces in lines

--- lua/includes/extensions/core/util.lua
+++ lua/includes/extensions/core/util.lua
@@ -1,7 +1,7 @@
 local Round = math.Round
 local SharedRandom = util.SharedRandom
 
-local match, Explode = string.match, string.Explode
+local match, Explode, Trim = string.match, string.Explode, string.Trim
 
 
 function util.SharedRandomInt( ... )
@@ -14,7 +14,7 @@ function util.ParseProperties( content, fn )
     local lines = Explode( "\n", content )
 
     for i = 1, #lines do
-        local key, value = match( lines[i], PATTERN_PROPERTY )
+        local key, value = match( Trim( lines[i] ), PATTERN_PROPERTY )
 
         if key and value then
             fn( key, value )
