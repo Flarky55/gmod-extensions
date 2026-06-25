ef50d4bb8852e279f6774097c49c4fedde46c5bf
Flarky55 <60645124+Flarky55@users.noreply.github.com>
2026-06-25 13:19:53 +0600

    `ENTITY:GetLookupAttachment` -> `ENTITY:FindAttachment`

--- lua/includes/extensions/core/entity.lua
+++ lua/includes/extensions/core/entity.lua
@@ -3,7 +3,7 @@ local ENTITY = FindMetaTable( "Entity" )
 local GetAttachment, LookupAttachment = ENTITY.GetAttachment, ENTITY.LookupAttachment
 
 
-function ENTITY:GetLookupAttachment( attachmentName )
+function ENTITY:FindAttachment( attachmentName )
     local id = LookupAttachment( self, attachmentName )
     if id <= 0 then return nil end
 
